43b0dba2aee0621e4a6cb3954cf4a4e80e3a19b8
Larsseee1 <elarsson2005@gmail.com>
2025-10-02 15:01:14 +0200

    fungerar

--- topmost.py
+++ topmost.py
@@ -3,33 +3,40 @@ import wordfreq
 import urllib.request
 
 def main():
-    
     lines_list = []
     if sys.argv[2][0] == 'h' and sys.argv[2][1] == 't' and sys.argv[2][2] == 't' and sys.argv[2][3] == 'p':
         response = urllib.request.urlopen(sys.argv[2])
-        lines_list = response.read().decode("utf-8").splitlines()
+        url_lines_list = response.read().decode("utf8").splitlines()
+        for lines in url_lines_list:
+            lines_list.append(lines)
+             
+    
         
-
             
     else:
         with open(sys.argv[2], encoding='utf-8') as input_file:
                 sys.argv[2] = input_file.readlines()
         for lines in sys.argv[2]:
+                print(lines)
+                print(lines.strip())
                 lines_list.append(lines)
     
     with open(sys.argv[1], encoding='utf-8') as input_file_2:
         sys.argv[1] = input_file_2.readlines()
     lines_list_stop = []
+    
     for lines in sys.argv[1]:
         lines_list_stop.append(lines.strip('\n'))
     #print(lines_list)
 
 
     token = wordfreq.tokenize(lines_list)
+    
     count = wordfreq.countWords(token, lines_list_stop)
+    
     most = wordfreq.printTopMost(count, int(sys.argv[3]))
     print(most)
-    
+    #print(sys.argv[2][0])
 main()
 
 
--- wordfreq.py
+++ wordfreq.py
@@ -1,31 +1,60 @@
 import sys
 
 def tokenize(my_list):
-    real_text = ''
-    text = ''.join(my_list)
-    for i in range(len(text)):
-        if i > 0 and text[i].isalpha() and text[i-1].isdigit():
-            real_text += ' ' + text[i]
-        elif not text[i].isalpha() and not text[i].isdigit():
-            real_text += ' ' + text[i] + ' '
-        else:
-            real_text += text[i]
-    return real_text.lower().split()
+   
+    words = []
+    for line in my_list:
+        start = 0
+        while start < len(line):
+            while start < len(line) and line[start].isspace():
+                start += 1
+            if start >= len(line):
+                break
+            if line[start].isdigit():
+                end = start
+                while end < len(line) and line[end].isdigit():
+                    end += 1
+                words.append(line[start:end])
+                start = end
+            elif line[start].isalpha():
+                end = start
+                while end < len(line) and line[end].isalpha():
+                    end += 1
+                words.append(line[start:end].lower())
+                start = end
+            else:
+                words.append(line[start])
+                start += 1
+    return words
+    #real_text = ''
+    #text = ''.join(my_list)
+    #for i in range(len(text)):
+    #    if text[i].isalpha() and text[i-1].isdigit():
+    #        real_text += ' ' + text[i]
+    #    elif not text[i].isalpha() and not text[i].isdigit():
+    #        real_text += ' ' + text[i] + ' '
+    #    else:
+    #        real_text += text[i]
+    #return real_text.lower().split()
 def countWords(words, stopWords):
     my_dict={}
-    ammount = 0
-    
-    
     for i in words:
-        ammount = 0
         if i not in stopWords:
-            for count in words:
-                if i == count:
-                    ammount +=1
-            my_dict[i] = ammount           
-        else:
-            continue       
+            my_dict.setdefault(i, 0)
+            my_dict[i] += 1
     return my_dict
+            
+    
+    #for i in words:
+    #    ammount = 0
+    #    if i not in stopWords:
+    #        for count in words and not my_dict[i]:
+    #            if i == count:
+    #                ammount +=1
+    #        my_dict[i] = ammount           
+    #    else:
+    #        continue       
+    #return my_dict
 def printTopMost(my_dict, n):
     
     sorted_dict = dict(sorted(my_dict.items(),key=lambda item: item[1], reverse=True))
