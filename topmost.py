97ba4f91fe4c89894c8035f4d2eb70c6c1614183
Larsseee1 <elarsson2005@gmail.com>
2025-10-02 13:46:13 +0200

    senaste

--- topmost.py
+++ topmost.py
@@ -1,23 +1,35 @@
 import sys
 import wordfreq
+import urllib.request
 
 def main():
+    
     lines_list = []
-    with open(sys.argv[2], encoding='utf-8') as input_file:
-        sys.argv[2] = input_file.readlines()
-    for lines in sys.argv[2]:
-        lines_list.append(lines)
+    if sys.argv[2][0] == 'h' and sys.argv[2][1] == 't' and sys.argv[2][2] == 't' and sys.argv[2][3] == 'p':
+        response = urllib.request.urlopen(sys.argv[2])
+        lines_list = response.read().decode("utf-8").splitlines()
+        
+
+            
+    else:
+        with open(sys.argv[2], encoding='utf-8') as input_file:
+                sys.argv[2] = input_file.readlines()
+        for lines in sys.argv[2]:
+                lines_list.append(lines)
     
     with open(sys.argv[1], encoding='utf-8') as input_file_2:
         sys.argv[1] = input_file_2.readlines()
     lines_list_stop = []
     for lines in sys.argv[1]:
         lines_list_stop.append(lines.strip('\n'))
-    print(lines_list)
+    #print(lines_list)
+
+
     token = wordfreq.tokenize(lines_list)
     count = wordfreq.countWords(token, lines_list_stop)
     most = wordfreq.printTopMost(count, int(sys.argv[3]))
     print(most)
+    
 main()
 
 
