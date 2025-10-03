ac1e936ba71c931e0f9dd63ed6d5d8b7c3c3aaed
04mohaa-ctrl <04mohaa@gmail.com>
2025-10-03 11:29:14 +0200

    rensat och renat

--- topmost.py
+++ topmost.py
@@ -4,38 +4,30 @@ import urllib.request
 
 def main():
     lines_list = []
-    if sys.argv[2].startswith('http'):
+    if sys.argv[2].startswith('http'):                            #Kontrollerar om input är en URL
         response = urllib.request.urlopen(sys.argv[2])
         url_lines_list = response.read().decode("utf8").splitlines()
         for lines in url_lines_list:
-            lines_list.append(lines)
-             
-        
-            
-    else:
+            lines_list.append(lines)        
+    else:                                                         #Om input är en fil
         with open(sys.argv[2], encoding='utf-8') as input_file:
                 sys.argv[2] = input_file.readlines()
         for lines in sys.argv[2]:
-                print(lines)
-                print(lines.strip())
                 lines_list.append(lines)
     
-    with open(sys.argv[1], encoding='utf-8') as input_file_2:
+    with open(sys.argv[1], encoding='utf-8') as input_file_2:     #Läser in stoppordsfilen
         sys.argv[1] = input_file_2.readlines()
     lines_list_stop = []
     
     for lines in sys.argv[1]:
         lines_list_stop.append(lines.strip('\n'))
-    #print(lines_list)
-
+   
 
     token = wordfreq.tokenize(lines_list)
-    
     count = wordfreq.countWords(token, lines_list_stop)
-    
     most = wordfreq.printTopMost(count, int(sys.argv[3]))
     print(most)
-    #print(sys.argv[2][0])
+    
 main()
 
 
--- wordfreq.py
+++ wordfreq.py
@@ -1,7 +1,7 @@
 import sys
 
 def tokenize(my_list):
-   
+   #Funktion för att dela upp text i ord
     words = []
     for line in my_list:
         start = 0
@@ -26,17 +26,8 @@ def tokenize(my_list):
                 words.append(line[start])
                 start += 1
     return words
-    #real_text = ''
-    #text = ''.join(my_list)
-    #for i in range(len(text)):
-    #    if text[i].isalpha() and text[i-1].isdigit():
-    #        real_text += ' ' + text[i]
-    #    elif not text[i].isalpha() and not text[i].isdigit():
-    #        real_text += ' ' + text[i] + ' '
-    #    else:
-    #        real_text += text[i]
-    #return real_text.lower().split()
-def countWords(words, stopWords):
+    
+def countWords(words, stopWords): #Räknar ord och exkluderar stoppord
     my_dict={}
     for i in words:
         if i not in stopWords:
@@ -44,36 +35,18 @@ def countWords(words, stopWords):
             my_dict[i] += 1
     return my_dict
             
-    
-    #for i in words:
-    #    ammount = 0
-    #    if i not in stopWords:
-    #        for count in words and not my_dict[i]:
-    #            if i == count:
-    #                ammount +=1
-    #        my_dict[i] = ammount           
-    #    else:
-    #        continue       
-    #return my_dict
-def printTopMost(my_dict, n):
-    
+def printTopMost(my_dict, n): #Skriver ut de n vanligaste orden
     sorted_dict = dict(sorted(my_dict.items(),key=lambda item: item[1], reverse=True))
     filtered_dict = list(sorted_dict.items())[:n]
-   
     for k, v in filtered_dict:
         print(str(k).ljust(19), str(v).rjust(5))
+
 def main():
     n = 2
     my_list = ''
-    
     tokenize(my_list)
-    
-    #with open('eng_stopwords.txt') as input_file:
-        #StopWords_text = "".join(input_file.readlines())
-        #stopWords = StopWords_text.split('\n')
     stopWords = []
     countWords(tokenize(my_list), stopWords)
-
     my_dict = countWords(tokenize(my_list), stopWords)
 
     printTopMost(my_dict, n)
